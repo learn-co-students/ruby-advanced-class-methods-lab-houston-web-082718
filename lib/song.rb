b8f9da7f9685426af4268013f64426f1a34b5440
sparkbold-git <sparkboldstudio@gmail.com>
2018-09-03 22:04:57 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,7 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
+
   @@all = []
 
   def self.all
@@ -10,4 +12,59 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = Song.new # same as self.new
+    song.name = song_name # instance of the song name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.create # create new song and save
+    song.name = song_name # assign the name to the song
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.find { |song| song.name == song_name }
+  end
+
+  def self.find_or_create_by_name(song_name)
+    unless song = self.find_by_name(song_name)
+    song =self.create_by_name(song_name)
+    end
+    song
+  end
+
+  def self.alphabetical
+    self.all.sort_by {|s| s.name}
+  end
+
+  def self.new_from_filename(file_name)
+    file_name = file_name.gsub(".mp3","").split(" - ")
+    song_name = file_name[1]
+    song = self.new
+    song.name = song_name
+    song.artist_name = file_name[0]
+    song
+  end
+
+  def self.create_from_filename(file_name)
+    file_name = file_name.gsub(".mp3","").split(" - ")
+    song_name = file_name[1]
+    song = self.create
+    song.name = song_name
+    song.artist_name = file_name[0]
+    song
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
 end
+
